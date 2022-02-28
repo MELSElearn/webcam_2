cd1072cd070acb261c3244edb6c7a90461f4b64c
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-02-28 14:51:45 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from base64 import b64decode, b64encode
 from cvzone.HandTrackingModule import HandDetector
-#from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import decode
 
 app = Flask(__name__)
 
