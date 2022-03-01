8df815558b4b35a50b17089de647883b99361b7e
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-03-01 13:57:56 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,9 +32,9 @@ def check_answer():
     if webaddress.find("http://answer:") >0:
         webaddress = webaddress[16:-1] # remove the first 16 and last character
         if webaddress=="cat":
-            cv2.putText(crop_img, str("Correct"), (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+            cv2.putText(crop_img, str("Correct"), (10, 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
         else:
-            cv2.putText(crop_img, str("Wrong"), (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+            cv2.putText(crop_img, str("Wrong"), (10, 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
     
     _, im_arr = cv2.imencode('.png', crop_img)
     im_bytes = im_arr.tobytes()
